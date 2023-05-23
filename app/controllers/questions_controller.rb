b0e6a97765d5843714c615161a8d0a9ff86e3b75
Daniel Scharf <id.scharfd@gmail.com>
2023-05-23 11:55:27 +0900

    added a .keep file to views

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,10 @@
 class QuestionsController < ApplicationController
+  response = OpenaiService.new('whatever you want to ask it').call
+
+    def new
+      @question = Question.new
+      @language = current_user.character.lanuage
+      @type = Map.find(params.id).monster_id
+    end
+
 end
