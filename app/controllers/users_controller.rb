076f295a2cb00c267b71ba5aea6b2ab3d5dd0d22
Tsubasa Toda <t.tsubasa310@gmail.com>
2023-05-23 12:25:26 +0900

    added ai_question column to question

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+  end
+
+  def create
+    @user = User.create
+
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :username)
+  end
 end
