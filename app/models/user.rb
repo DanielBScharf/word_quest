496cd82c956a05c6010d57d44533ae96b6a0ac75
XZaiver Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2023-05-23 15:25:54 +0900

    remove user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :first_name, presence: true, on: :create
-  validates :last_name, presence: true, on: :create
-  validates :username, presence: true, uniqueness: true, on: :create
+  # validates :first_name, presence: true, on: :create
+  # validates :last_name, presence: true, on: :create
+  # validates :username, presence: true, uniqueness: true, on: :create
   has_many :characters
 end
