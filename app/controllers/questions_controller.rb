1c4483e5ab79397a20e8cb6ba6ad5ab9fafb687f
Tsubasa Toda <128109353+Tsubasa-Toda@users.noreply.github.com>
2023-05-23 11:35:13 +0900

    Update questions_controller.rb

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,19 +1,2 @@
 class QuestionsController < ApplicationController
-  def show
-    @question = Question.find(params[:id])
-  end
-
-  def new
-    @question = Question.new
-  end
-
-  def create
-    @question = Question.new(question_params)
-  end
-
-  private
-
-  def question_params
-    params.require(:question).permit(:time, :answer_id, :question_id, :character_id)
-  end
 end
