e56d84c642f99174841b3a622f952df6fe784c90
stefano zerega <stefano.japan93@gmail.com>
2023-05-26 15:50:34 +0900

    hint last momment fix

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -3,19 +3,10 @@ class MonstersController < ApplicationController
    @monster = Monster.find(params[:id])
 
     # creates a question so we can generate the question when the monster is called
-    @character = Character.find_by(user_id: current_user.id)
-    @question = Question.new
-    @question.monster = @monster
-
-    @question.category = @monster.category
-    response = openapi.split(',', 3)
-    @question.text = response[0].tr('"', '')
-    @choices = response[2].tr('/([|]|")/', '').split(',')
-    @answer = response[1].tr('"', '')
-
+    
   end
 
-    @monster = Monster.find(params[:id])
+    private
 
     respond_to do |format|
       format.html
