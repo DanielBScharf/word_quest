0001fb3e3ec5327ce5038bbf2037d6290fb9ceec
Daniel Scharf <id.scharfd@gmail.com>
2023-05-29 13:48:02 +0900

    got the health bar to work and to save, tried to troubleshoot the crazy loop with the questions. will have to downgrade our map to something more static but works.

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -4,11 +4,11 @@ class MapsController < ApplicationController
     @map = Map.all.first
     @monster = Monster.all.first
     @map = @monster.map
-
     respond_to do |format|
       format.html
       format.json { render json: { id: @monster.id } }
     end
+    @health = set_character.current_health
   end
   def new
     num_monster = rand(3..5)
@@ -27,6 +27,8 @@ class MapsController < ApplicationController
   end
 
   def show_village
+    @character.current_health = @character.max_health
+    @character.update(current_health: @character.current_health)
   end
 
   private
