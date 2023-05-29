57c32e85f2761bf5980c1005879661681e3337a7
XZaiver Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2023-05-29 14:06:37 +0900

    clean files and methods

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -37,13 +37,13 @@ class QuestionsController < ApplicationController
   private
 
   def openapi
-    p prompt = 'Respond in JSON form and include no other commentary, JSON object should be as follows {"question": "", "answer": "", "choices": [] } . Give me a CEFR A' + @character.level.to_s + ' ' + @character.language + ' vocabulary question with four multiple choices. None of the choices can be synonyms of each other. Ensure only one of the multiple choices is the correct answer.'
+    prompt = 'Respond in JSON form and include no other commentary, JSON object should be as follows {"question": "", "answer": "", "choices": [] } . Give me a CEFR A' + @character.level.to_s + ' ' + @character.language + ' vocabulary question with four multiple choices. The choices cannot be synonyms of each other. Ensure only one of the multiple choices is the correct answer.'
     OpenaiService.new(prompt).call
   end
 
   def answers(choices)
-    choices.map.each do |answer|
-      Answer.create(text: answer.strip, correct: @answer.match?(answer.strip), question_id: @question.id)
+    choices.map.each do |choice|
+      Answer.create(text: choice, correct: @answer.match?(choice), question_id: @question.id)
     end
   end
 
