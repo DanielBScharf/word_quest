3d9e49e789e6633ad79f31cdf8ef3ad6bddb5ac4
Daniel Scharf <id.scharfd@gmail.com>
2023-05-30 19:16:58 +0900

    broke everything trying to get health to work. will return to it tomorrow

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -3,6 +3,7 @@ class MonstersController < ApplicationController
   # need to fix this to show the information on a specific monster type
 
   @monster = Monster.find(params[:id])
+
   # creates a question so we can generate the question when the monster is called
   @character = Character.find_by(user_id: current_user.id)
   end
