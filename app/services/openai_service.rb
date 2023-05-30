9db106d2ca8571d0579aec97bcfbedcd73764234
XZaiver Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2023-05-30 14:07:50 +0900

    changed requence

--- app/services/openai_service.rb
+++ app/services/openai_service.rb
@@ -4,20 +4,20 @@ require 'json'
 class OpenaiService
   attr_reader :client, :prompt
 
-	def initialize(prompt)
+  def initialize(prompt)
     @client = OpenAI::Client.new
     @prompt = prompt
   end
 
   def call
-    response = client.completions(
-      parameters: {
-        model: "text-davinci-003",
-        prompt: prompt,
-        max_tokens: 2000,
-        temperature: 0.7,
-        top_p: 1
-      })
+    response = client.completions(parameters: { model: "text-davinci-003",
+                                                prompt: @prompt,
+                                                max_tokens: 256,
+                                                temperature: 1,
+                                                top_p: 1,
+                                                best_of: 1,
+                                                frequency_penalty: 0,
+                                                presence_penalty: 0.1 })
     JSON.parse(response['choices'][0]['text'])
   end
 end
