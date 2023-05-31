1d2b73c450c0c4b2bbc6bd23005142dd5194d18b
XZaiver Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2023-05-31 16:15:53 +0900

    now saves username, firstname,lastname

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   root to: "pages#welcome"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/home", to: "pages#home", as: :home
