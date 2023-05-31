23bfca1ffa6163395c161b5de0e2de37e0523fbd
Tsubasa Toda <128109353+Tsubasa-Toda@users.noreply.github.com>
2023-05-31 12:43:36 +0900

    new landing page (#132)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "pages#welcome"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "/home", to: "pages#home", as: :home
   resources :users do
     resources :characters
   end
